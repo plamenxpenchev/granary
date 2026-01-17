1184f42573056330d87a98ca4403a72ce800c6eb
Plamen Penchev <plamenxpenchev@gmail.com>
2026-01-18 00:36:20 +0200

    Add database test

---
+++ mvn-test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+if [ "$#" -ne 1 ]; then
+    echo "Expected 1 parameter to script: the path to the age secret key file (SOPS_AGE_KEY_FILE)."
+    exit 1
+fi
+
+GRANARY_ROOT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+SOPS_ENV_FILE=${GRANARY_ROOT_DIR}/.sops.env
+SOPS_GRANARY_PRS_FILE=${GRANARY_ROOT_DIR}/properties/src/main/resources/sops.granary.prs
+
+if [ ! -f ${SOPS_ENV_FILE} ]; then
+    echo "${SOPS_ENV_FILE} not found."
+    exit 1
+fi
+
+if [ ! -f ${SOPS_GRANARY_PRS_FILE} ]; then
+    echo "${SOPS_GRANARY_PRS_FILE} not found."
+    exit 1
+fi
+
+ENV_FILE=${GRANARY_ROOT_DIR}/.env
+GRANARY_PRS_FILE=${GRANARY_ROOT_DIR}/properties/src/main/resources/granary.prs
+
+export SOPS_AGE_KEY_FILE=$1
+
+if [ ! -f ${SOPS_AGE_KEY_FILE} ]; then
+    echo "${SOPS_AGE_KEY_FILE} not found."
+    exit 1
+fi
+
+sops decrypt --input-type dotenv --output-type dotenv ${SOPS_ENV_FILE} > ${ENV_FILE}
+sops decrypt --input-type dotenv --output-type dotenv ${SOPS_GRANARY_PRS_FILE} > ${GRANARY_PRS_FILE}
+
+cd ${GRANARY_ROOT_DIR}
+mvn clean install
--- server/src/test/java/org/granary/server/GranaryTest.java
+++ server/src/test/java/org/granary/server/GranaryTest.java
@@ -1,4 +1,57 @@
 package org.granary.server;
 
-public class GranaryTest {
+import org.granary.database.DatabaseTest;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GranaryTest extends DatabaseTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GranaryTest.class);
+
+    @Test
+    public void basicTest() throws SQLException {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = connectionPool.getConnection();
+
+            ps = conn.prepareStatement("CREATE TABLE test_table(test_field_1 VARCHAR(255), test_field_2 INT)");
+            ps.executeUpdate();
+            ps.close();
+
+            ps = conn.prepareStatement("INSERT INTO test_table(test_field_1, test_field_2) " +
+                    "VALUES ('val1', 1), ('val2', 2)");
+            ps.executeUpdate();
+            ps.close();
+
+            ps = conn.prepareStatement("SELECT COUNT(*) FROM test_table");
+            rs = ps.executeQuery();
+            int count = 0;
+            if (rs.next()) {
+                count = rs.getInt(1);
+            }
+            assertEquals(2, count);
+            LOG.info("Two results found - Success.");
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+    }
 }
